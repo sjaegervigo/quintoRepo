436075fb1b652a7158898001f9859a7b496b2b5e
Sebastian Jaeger <sjaegervigo@gmail.com>
2025-04-12 19:22:30 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                                    # Nombre del paquete
+    version="0.1.0",                                      # Versión inicial
+    packages=find_packages(),                             # Paquetes a incluir
+    description="Un paquete pip simple de saludo",        # Breve descripción
+    author="Sebastian Jaeger",                            # Tu nombre
+    author_email="sjaegervigo@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/sjaegervigo/quitoRepo",       # URL del proyecto
+)
